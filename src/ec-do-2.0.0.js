9b39c00b44b4f50a794db9c62cce652a721417c7
chui-web <158874645@qq.com>
2018-01-16 20:22:47 +0800

    优化removeClass函数

--- src/ec-do-2.0.0.js
+++ src/ec-do-2.0.0.js
@@ -801,17 +801,18 @@ let ecDo = {
     },
     //删除类名
     removeClass(obj, classStr) {
+        let reg;
         if ((this.istype(obj, 'array') || this.istype(obj, 'elements')) && obj.length > 1) {
             for (let i = 0, len = obj.length; i < len; i++) {
                 if (this.hasClass(obj[i], classStr)) {
-                    let reg = new RegExp('(\\s|^)' + classStr + '(\\s|$)');
+                    reg = new RegExp('(\\s|^)' + classStr + '(\\s|$)');
                     obj[i].className = obj[i].className.replace(reg, '');
                 }
             }
         }
         else {
             if (this.hasClass(obj, classStr)) {
-                let reg = new RegExp('(\\s|^)' + classStr + '(\\s|$)');
+                reg = new RegExp('(\\s|^)' + classStr + '(\\s|$)');
                 obj.className = obj.className.replace(reg, '');
             }
         }
