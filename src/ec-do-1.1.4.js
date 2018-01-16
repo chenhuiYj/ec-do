6185a7f60b50357ae96612084ae05467964f9a3a
chui-web <158874645@qq.com>
2018-01-16 20:19:25 +0800

    优化getCookie函数

--- src/ec-do-1.1.4.js
+++ src/ec-do-1.1.4.js
@@ -691,9 +691,9 @@ var ecDo = {
     },
     //获取cookie
     getCookie: function (name) {
-        var arr = document.cookie.split('; ');
+        var arr = document.cookie.split('; '),arr2;
         for (var i = 0; i < arr.length; i++) {
-            var arr2 = arr[i].split('=');
+            arr2 = arr[i].split('=');
             if (arr2[0] == name) {
                 return arr2[1];
             }
