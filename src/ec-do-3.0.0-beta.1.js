1994d206a9aa2c02bf6bd072eb9a74876e14f1ea
chui-web <158874645@qq.com>
2018-05-21 18:02:28 +0800

    3.0.0-setUrlPrmt和getUrlPrmt函数名修改（setUrlParam和getUrlParam）

--- src/ec-do-3.0.0-beta.1.js
+++ src/ec-do-3.0.0-beta.1.js
@@ -630,9 +630,9 @@ let ecDo = {
         }
     },
     //设置url参数
-    //setUrlPrmt({'a':1,'b':2})
+    //setUrlParam({'a':1,'b':2})
     //result：a=1&b=2
-    setUrlPrmt(obj) {
+    setUrlParam(obj) {
         let _rs = [];
         for (let p in obj) {
             if (obj[p] != null && obj[p] != '') {
@@ -642,9 +642,9 @@ let ecDo = {
         return _rs.join('&');
     },
     //获取url参数
-    //getUrlPrmt('segmentfault.com/write?draftId=122000011938')
+    //getUrlParam('segmentfault.com/write?draftId=122000011938')
     //result：Object{draftId: "122000011938"}
-    getUrlPrmt(url) {
+    getUrlParam(url) {
         url = url ? url : window.location.href;
         let _pa = url.substring(url.indexOf('?') + 1),
             _arrS = _pa.split('&'),
