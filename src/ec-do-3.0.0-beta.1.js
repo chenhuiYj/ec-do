3768e82139c0247ba4ca4681e5ee83d84f5ec0da
chui-web <158874645@qq.com>
2018-11-16 19:34:12 +0800

    测试 loadImg

--- src/ec-do-3.0.0-beta.1.js
+++ src/ec-do-3.0.0-beta.1.js
@@ -1187,7 +1187,9 @@ let ecDo = {
             dom.onerror=dom.onload=function () {
                 now++
                 console.log(now,len)
+                //如果还有图片待加载
                 if(_dom.length>0){
+                    //递归调用加载
                     handleLoad(_dom.shift());
                 }
                 else{
