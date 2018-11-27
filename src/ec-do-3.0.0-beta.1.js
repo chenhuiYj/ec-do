2c0f280829881a945e6009659b7d9b0b30d8b0b8
chui-web <158874645@qq.com>
2018-11-27 10:47:54 +0800

    修改 show 函数的沉余代码

--- src/ec-do-3.0.0-beta.1.js
+++ src/ec-do-3.0.0-beta.1.js
@@ -1042,7 +1042,7 @@ let ecDo = {
      * @return {ecDo}
      */
     show(obj){
-        let blockArr = ['div', 'li', 'ul', 'ol', 'dl', 'table', 'article', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'hr', 'header', 'footer', 'details', 'summary', 'section', 'aside', '']
+        let blockArr = ['div', 'li', 'ul', 'ol', 'dl', 'table', 'article', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'hr', 'header', 'footer', 'details', 'summary', 'section', 'aside']
         if (blockArr.indexOf(obj.tagName.toLocaleLowerCase()) === -1) {
             obj.style.display = 'inline';
         }
