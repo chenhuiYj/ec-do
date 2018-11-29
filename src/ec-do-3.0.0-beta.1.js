575364b68077a253c38871c0e182730d41f35e55
chui-web <158874645@qq.com>
2018-11-29 10:51:28 +0800

    getUrlParam 更改私有变量名称

--- src/ec-do-3.0.0-beta.1.js
+++ src/ec-do-3.0.0-beta.1.js
@@ -677,16 +677,16 @@ let ecDo = {
      */
     getUrlParam(url) {
         url = url ? url : window.location.href;
-        let _pa = url.substring(url.indexOf('?') + 1),
-            _arrS = _pa.split('&'),
+        let _param = url.substring(url.indexOf('?') + 1),
+            _paramItem = _param.split('&'),
             _rs = {};
-        for (let i = 0, _len = _arrS.length; i < _len; i++) {
-            let pos = _arrS[i].indexOf('=');
+        for (let i = 0, _len = _paramItem.length; i < _len; i++) {
+            let pos = _paramItem[i].indexOf('=');
             if (pos == -1) {
                 continue;
             }
-            let name = _arrS[i].substring(0, pos),
-                value = window.decodeURIComponent(_arrS[i].substring(pos + 1));
+            let name = _paramItem[i].substring(0, pos),
+                value = window.decodeURIComponent(_paramItem[i].substring(pos + 1));
             _rs[name] = value;
         }
         return _rs;
