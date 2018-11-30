d8855682147ee052209ce017fb729c400541db25
chui-web <158874645@qq.com>
2018-11-30 10:45:30 +0800

    getFontSize 更换底油变量命名

--- src/ec-do-3.0.0-beta.1.js
+++ src/ec-do-3.0.0-beta.1.js
@@ -561,7 +561,7 @@ let ecDo = {
             win = window;
         let docEl = doc.documentElement,
             resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-            recalc = function () {
+            countSize = function () {
                 let clientWidth = docEl.clientWidth;
                 if (!clientWidth) return;
                 //如果屏幕大于750（750是根据我效果图设置的，具体数值参考效果图），就设置clientWidth=750，防止font-size会超过100px
@@ -572,9 +572,9 @@ let ecDo = {
                 docEl.style.fontSize = 100 * (clientWidth / _client) + 'px';
             };
         //屏幕大小改变，或者横竖屏切换时，触发函数
-        win.addEventListener(resizeEvt, recalc, false);
+        win.addEventListener(resizeEvt, countSize, false);
         //文档加载完成时，触发函数
-        doc.addEventListener('DOMContentLoaded', recalc, false);
+        doc.addEventListener('DOMContentLoaded', countSize, false);
     },
     /**
      * @description 到某一个时间的倒计时
