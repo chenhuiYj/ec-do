a0ebfc2c52aeaa67f6c19841d4e8123056c309c9
chui-web <158874645@qq.com>
2018-12-03 14:10:08 +0800

    checkPwdLevel 函数去除长度限制

--- src/ec-do-3.0.0-beta.1.js
+++ src/ec-do-3.0.0-beta.1.js
@@ -157,9 +157,9 @@ let ecDo = {
      */
     checkPwdLevel(str) {
         let nowLv = 0;
-        if (str.length < 6) {
-            return nowLv
-        }
+        // if (str.length < 6) {
+        //     return nowLv
+        // }
         let rules = [/[0-9]/, /[a-z]/, /[A-Z]/, /[\.|-|_]/];
         for (let i = 0; i < rules.length; i++) {
             if (rules[i].test(str)) {
