27300a5ab1f4b63129111643ac9c3955e5a85e58
chui-web <158874645@qq.com>
2018-12-05 14:57:20 +0800

    增加 fnIndex.js 可以自动生成序号

--- fnIndex.js
+++ fnIndex.js
@@ -9,19 +9,16 @@ fs.readFile('./src/ec-do-3.0.0-beta.1.js', (err, data) => {
         let md="";
         let obj=new Function(data.toString().replace('let','let _ecObj=')+';return _ecObj')();
         Object.keys(obj).forEach((item,index)=>{
-            //md+=`## ${index+1}.${item}\r\n`
             fns.push({
                 index:index,
                 name:item
             })
         })
-        //fs.writeFileSync("./README.md", md);
     }
 })
 
 fs.readFile('./README.md', (err, data) => {
     if (err) {
-        //如 果 出 错， 输 出 错 误 日 志， 并 给 客 户 端 返 回“ Server Error”
         console.error(err);
     }
     else {
