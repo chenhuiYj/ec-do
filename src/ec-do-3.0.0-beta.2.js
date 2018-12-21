99712b6132ecb2af0968d88176ea3964c405a031
chui-web <158874645@qq.com>
2018-12-21 10:00:41 +0800

    优化 filterParams 函数

--- src/ec-do-3.0.0-beta.2.js
+++ src/ec-do-3.0.0-beta.2.js
@@ -639,12 +639,21 @@ let ecDo = (function () {
         /**
          * @description 清除对象中值为空的属性
          * @param obj
+         * @param keepValues
          * @return {{}}
          */
-        filterParams(obj) {
+        filterParams(obj,keepValues=[0,false]) {
+            keepValues.forEach((item,index)=>{keepValues[index]=Number.isNaN(item)?'NaN':item});
+            function _checkFalse(val) {
+                let _val=val;
+                if(Number.isNaN(val)){
+                    _val='NaN'
+                }
+                return keepValues.indexOf(_val)!==-1;
+            }
             let _newPar = {};
             for (let key in obj) {
-                if ((obj[key] === 0 || obj[key] === false || obj[key]) && obj[key].toString().replace(/(^\s*)|(\s*$)/g, '') !== '') {
+                if ( _checkFalse(obj[key])|| (obj[key]&&obj[key].toString().replace(/(^\s*)|(\s*$)/g, '') !== '')) {
                     _newPar[key] = obj[key];
                 }
             }
