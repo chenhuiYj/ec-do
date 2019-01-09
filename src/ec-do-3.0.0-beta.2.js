e2c6f7a4897fe7e0eb1fc62bb70eb3b125791124
chui-web <158874645@qq.com>
2019-01-09 18:07:25 +0800

    删除多余代码

--- src/ec-do-3.0.0-beta.2.js
+++ src/ec-do-3.0.0-beta.2.js
@@ -160,9 +160,6 @@ let ecDo = (function () {
          */
         checkPwdLevel(str) {
             let nowLv = 0;
-            // if (str.length < 6) {
-            //     return nowLv
-            // }
             let rules = [/[0-9]/, /[a-z]/, /[A-Z]/, /[\.|-|_]/];
             for (let i = 0; i < rules.length; i++) {
                 if (rules[i].test(str)) {
